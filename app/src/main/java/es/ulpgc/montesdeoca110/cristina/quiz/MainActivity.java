6d8a27ecd0eac63d88802b49c82192153273eb1e
Tina997 <cristina.montesdeoca110@alu.ulpgc.es>
2019-02-06 13:50:30 +0000

    Funcionalidad basica en java

--- app/src/main/java/es/ulpgc/montesdeoca110/cristina/quiz/MainActivity.java
+++ app/src/main/java/es/ulpgc/montesdeoca110/cristina/quiz/MainActivity.java
@@ -2,12 +2,36 @@ package es.ulpgc.montesdeoca110.cristina.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView answerText, questionText;
+    private Button trueButton, falseButton,cheatButton, nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //fijar el layout de la activity
         setContentView(R.layout.activity_main);
+        //accerder a los componentes del layout
+        answerText = findViewById(R.id.answerText);
+        questionText = findViewById(R.id.questionText);
+        trueButton = findViewById(R.id.trueButton);
+        falseButton = findViewById(R.id.falseButton);
+        cheatButton = findViewById(R.id.cheatButton);
+        nextButton = findViewById(R.id.nextButton);
+
+    }
+    public void onTrueButtonClicked(View view){
+        Log.e("MainActivity","onTrueButtonClicked()");
+        answerText.setText(R.string.trueButtonLabel);
+    }
+    public void onFalseButtonClicked(View view){
+        Log.e("MainActivity","onFalseButtonClicked()");
+        answerText.setText(R.string.falseButtonLabel);
     }
 }
